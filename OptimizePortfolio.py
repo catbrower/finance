7f7fcc5146f2239624d789d5d59f5c746a2b1e50
Catherine Brower <catherinemb42@gmail.com>
2019-10-29 14:32:39 -0400

    fixing python2 -> python3 issues

--- OptimizePortfolio.py
+++ OptimizePortfolio.py
@@ -19,7 +19,7 @@ def portfolio_annualised_performance(weights, mean_returns, cov_matrix):
 def random_portfolios(num_portfolios, mean_returns, cov_matrix, risk_free_rate):
     results = np.zeros((3,num_portfolios))
     weights_record = []
-    for i in xrange(num_portfolios):
+    for i in range(num_portfolios):
         weights = np.random.random(4)
         weights /= np.sum(weights)
         weights_record.append(weights)
