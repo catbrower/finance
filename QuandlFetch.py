f034b72e96e044dfd26a5dc8248efe9da16f10bd
Catherine Brower <catherinemb42@gmail.com>
2020-01-04 19:20:56 -0500

    improving portfolio calculation

--- QuandlFetch.py
+++ QuandlFetch.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import quandl
 import numpy
 
@@ -6,9 +7,15 @@ quandl.ApiConfig.api_key = "www.quandl.com"
 with open("quandl.key", "r") as f:
     quandl.ApiConfig.api_key = f.read().strip()
 
+def getStock(ticker, startDate, endDate):
+    return 
+
 def getStocks(stockArray, startDate = '2016-1-1', endDate = '2017-12-31' ):
     data = quandl.get_table('WIKI/PRICES', ticker = stockArray,
     qopts = { 'columns': ['date', 'ticker', 'adj_close'] },
     date = { 'gte': startDate, 'lte': endDate }, paginate=True)
+
+    data = data.dropna()
+
     df = data.set_index('date')
     return df.pivot(columns='ticker')
