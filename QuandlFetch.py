1636af51344a62c08947c52d04fffd9095be6a85
Catherine Brower <catherinemb42@gmail.com>
2020-02-12 00:54:52 -0500

    Fixed numerous data issues

--- QuandlFetch.py
+++ QuandlFetch.py
@@ -1,18 +1,33 @@
 import pandas as pd
 import quandl
 import numpy
+import sys
 
 quandl.ApiConfig.api_key = "www.quandl.com"
+TICKER_LIMIT = 50
 
 with open("quandl.key", "r") as f:
     quandl.ApiConfig.api_key = f.read().strip()
 
-def getStocks(stockArray, startDate = '2016-1-1', endDate = '2017-12-31'):
+def get_data(path = "data/stock_prices.csv"):
+    return pd.read_csv(path, parse_dates=True, index_col="date")
+
+def getStocks(stockArray, startDate, endDate = None):
+    if(len(stockArray) > TICKER_LIMIT):
+        print("QuandlFetch.getStocks is limited to 50 stocks")
+        sys.exit()
+
+    endDate = startDate if endDate == None else endDate
+    
     data = quandl.get_table('WIKI/PRICES', ticker = stockArray,
-    qopts = { 'columns': ['date', 'ticker', 'adj_close'] },
-    date = { 'gte': startDate, 'lte': endDate }, paginate=True)
+        qopts = { 'columns': ['date', 'ticker', 'adj_close'] },
+        date = { 'gte': startDate, 'lte': endDate }, paginate=True)
 
-    data = data.dropna()
-
-    df = data.set_index('date')
-    return df.pivot(columns='ticker')
+    if(data.empty):
+        return data
+    else:
+        df = data.set_index('date')
+        df = df.pivot(columns='ticker')['adj_close']
+        df.to_csv('data/tmp_data.csv')
+        df = pd.read_csv('data/tmp_data.csv', parse_dates=True, index_col="date")
+        return df
